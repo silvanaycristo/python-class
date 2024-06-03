85963c46a0c851183e1209d1f3fab11e2470f2d9
silvanaycristo <silvanac@lcg.unam.mx>
2024-06-03 08:30:19 -0600

    Se actualizaron detalles del template "buscador_codones.py"

--- curso-biopython/buscador_codones.py
+++ curso-biopython/buscador_codones.py
@@ -1,29 +1,33 @@
 '''
 NAME: 
-       Buscador de Codones para marcos de lectura
+       Buscador de codones para marcos de lectura
 
 VERSION: 1.0
 
-AUTHOR: Silvana Martinez
+AUTHOR: Silvana Yalú Cristo Martínez
 
-DESCRIPCIÓN: 
+DESCRIPTION: 
+
         Este script lee un archivo FASTA que contiene secuencias de ADN y encuentra todos los codones para los 6 marcos de lectura.
         Los codones para cada marco de lectura se imprimen en formato FASTA en archivos separados nombrados según los marcos.
 
 CATEGORY:
+
         Análisis de secuencias de ADN/Bioinformática
 
 USAGE:
 
-    % python buscador_codones.py -i seq.nt.fa
+     % python buscador_codones.py -i seq.nt.fa
     
 
 ARGUMENTS:
-    -i : Archivo FASTA de entrada que contiene las secuencias de ADN.
 
-METHOD:
-    El script usa SeqIO de Biopython para leer las secuencias del archivo de entrada y encontrar los codones para los seis marcos de lectura.
-    Genera una función que toma el marco de lectura, la secuencia y el nombre del archivo de salida como parámetros y llama a esta función para cada secuencia.
+         -i : Archivo FASTA de entrada que contiene las secuencias de ADN.
+
+METHOD: 
+
+        El script usa SeqIO de Biopython para leer las secuencias del archivo de entrada y encontrar los codones para los seis marcos de lectura.
+        Genera una función que toma el marco de lectura, la secuencia y el nombre del archivo de salida como parámetros y llama a esta función para cada secuencia.
 
 '''
 
@@ -60,17 +64,18 @@ def find_codons(frame, sequence, output_file_name):
 # =                            main
 # ===========================================================================
 if __name__ == "__main__":
-    # Leer el archivo de entrada
+    # Lee el archivo de entrada
     input_file = args.input
     sequences = SeqIO.parse(input_file, "fasta")
     
-    # Procesar cada secuencia
+    # Procesa cada secuencia
     for record in sequences:
+        seq_id = record.id
         seq = str(record.seq)
-        seq_obj = Seq(seq)  # Convertir la cadena en un objeto Seq
-        # Paso 1 y 2: Procesar los 6 marcos de lectura
+        seq_obj = Seq(seq)  # Convierte la cadena en un objeto Seq
+        # Paso 1 y 2: Procesa los 6 marcos de lectura
         for frame in range(3):
-            find_codons(frame, seq, f"Frame_{frame+1}.fa")
-            find_codons(frame, seq_obj.reverse_complement(), f"Frame_{frame+4}.fa")
+            find_codons(frame, seq, f"{seq_id}_Frame_{frame+1}.fa")
+            find_codons(frame, seq_obj.reverse_complement(), f"{seq_id}_Frame_{frame+4}.fa")
 
-    print("Proceso completado, puedes verificar los archivos de salida generados en el directorio donde ejecutaste el script.")  # Mensaje de confirmaciÃ³n final
+    print("Proceso completado, puedes verificar los archivos de salida generados en el directorio donde ejecutaste el script.")  # Mens
